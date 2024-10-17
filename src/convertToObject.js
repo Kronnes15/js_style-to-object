88f6efd10f37f15610fc5d680fa4086b31a8a0a4
Misha <banderwolfe06@gmail.com>
2024-10-17 14:07:27 +0200

    Update HTML and fix image paths

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .filter((declaration) => declaration.trim().length > 0)
+    .map((declaration) => {
+      const [property, value] = declaration
+        .split(':')
+        .map((item) => item.trim());
+      const camelCasedProperty = property.replace(
+        /-([a-z])/g,
+        (match, letter) => letter.toUpperCase(),
+      );
+
+      return { [camelCasedProperty]: value };
+    })
+    .reduce((accumulator, current) => Object.assign(accumulator, current), {});
 }
 
 module.exports = convertToObject;
